42a4a3285a609e5c4b19842255827137a1acc82a
mert.dogan <mert.dogan@E07-18>
2026-05-19 15:09:19 +0200

    Backend change

---
+++ backend/Fitness/Fitness/Controllers/KursController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fitness.Controllers
+{
+    public class KursController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
