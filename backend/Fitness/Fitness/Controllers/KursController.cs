24033df4422bafb6b7292e06c2c068fc427a7d24
mert.dogan <mert.dogan@E07-18>
2026-05-22 11:16:25 +0200

    Füge Delete-Methode für Kurse hinzu
    Die Methode `DeleteKurs` wurde in der Klasse `KursController` implementiert, um das Löschen eines Kurses anhand seiner ID zu ermöglichen.
    
    - Mit `[HttpDelete]` wird die Methode für DELETE-Anfragen bereitgestellt.
    - Gibt `NotFound` zurück, wenn der Kurs nicht existiert.
    - Entfernt den Kurs aus dem Kontext und speichert die Änderungen in der Datenbank.
    - Behandelt potenzielle Fehler beim Speichern mit einer `BadRequest`-Antwort.
    - Gibt den gelöschten Kurs bei Erfolg zurück.

--- backend/Fitness/Fitness/Controllers/KursController.cs
+++ backend/Fitness/Fitness/Controllers/KursController.cs
@@ -37,5 +37,29 @@ namespace Fitness.Controllers
         {
             return Ok(await _context.Kurse.ToListAsync());
         }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteKurs(int id)
+        {
+            Kurs? kurs = await _context.Kurse.FindAsync(id);
+
+            if (kurs == null)
+            {
+                return NotFound($"Kurs mit der ID {id} konnte nicht gefunden werden");
+            }
+
+            _context.Kurse.Remove(kurs);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            } catch (DbUpdateException ex)
+            {
+                Console.WriteLine(ex);
+                return BadRequest($"Error beim löschen des Kurses. {ex}");
+            }
+
+            return Ok(kurs);
+        }
     }
 }
